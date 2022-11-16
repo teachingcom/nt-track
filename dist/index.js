6643483099fcea939d3231f8774a396b36984b71
hugo <hugo@hugoware.net>
2022-11-16 14:50:05 -0600

    also save lane

--- dist/index.js
+++ dist/index.js
@@ -105384,6 +105384,7 @@ var TrackView = /*#__PURE__*/function (_BaseView) {
     });
     (0, _defineProperty2.default)((0, _assertThisInitialized2.default)(_this), "lanes", []);
     (0, _defineProperty2.default)((0, _assertThisInitialized2.default)(_this), "reserveLane", function (preferredLane, id) {
+      _this.lanes[preferredLane] = id;
       return preferredLane;
       var MAX_LANES = 5;
 
--- src/views/track/index.js
+++ src/views/track/index.js
@@ -179,8 +179,9 @@ export default class TrackView extends BaseView {
 
 	// reserve a new lane
 	reserveLane = (preferredLane, id) => {
+		this.lanes[preferredLane] = id
 		return preferredLane
-		
+
 		const MAX_LANES = 5;
 		for (let lane = 0; lane < MAX_LANES; lane++) {
 			if (!this.lanes[lane]) {
