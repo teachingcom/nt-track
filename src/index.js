f73ab15566e91e1901de75d60916d4542fe0ee09
hugo <hugo@teaching.com>
2023-09-14 14:37:22 -0500

    updated track version number

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ export { default as Animation } from './views/animation'
 export { default as NameCard } from './views/namecard'
 export const Audio = AudioController
 
-try { window.NTTRACK = '2.2.8' } catch (ex) { }
+try { window.NTTRACK = '2.2.9' } catch (ex) { }
